f9350e558afb2ca5015cad3c41966e725d6bf24b
Jullia <jullia.pires@cogecom.com>
2026-05-26 16:36:47 -0300

    Primeiro commit

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    # Esse teste tem 3 etapas (AAA)
+    # A: Arrange - Arranjo
+    # A: Act     - Executa a coisa (o SUT - System Under Test)
+    # A: Assert  - Garanta que A é A
+
+    # arrange
+    client = TestClient(app)
+
+    # act
+    response = client.get('/')
+
+    assert response.json() == {'message': 'Olá mundo!'}
+    assert response.status_code == HTTPStatus.OK
