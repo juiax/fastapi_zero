dea41407cd150f33f67983b5bc279c276e7b6a0e
Jullia <jullia.pires@cogecom.com>
2026-06-01 10:04:52 -0300

    Primeira versao

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,77 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_root_deve_retornar_ola_mundo():
+def test_root_deve_retornar_ola_mundo(client):
     # Esse teste tem 3 etapas (AAA)
     # A: Arrange - Arranjo
     # A: Act     - Executa a coisa (o SUT - System Under Test)
     # A: Assert  - Garanta que A é A
-
     # arrange
-    client = TestClient(app)
-
     # act
     response = client.get('/')
 
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user(client):
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'juia',
+            'email': 'juia@email.com',
+            'password': 'juiasenhas',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'juia@email.com',
+        'username': 'juia',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'juia',
+                'email': 'juia@email.com',
+            },
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
