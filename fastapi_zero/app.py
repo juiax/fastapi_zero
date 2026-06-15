a56bd1e51b2db969e22d298b5182393eb1829f7e
Jullia <jullia.pires@cogecom.com>
2026-06-15 14:42:50 -0300

    feat(tasks): implementa endpoints de tarefas

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,13 +2,14 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.routers import auth, users
+from fastapi_zero.routers import auth, todo, users
 from fastapi_zero.schemas import Message
 
 app = FastAPI(title='Minha primeira API!', debug=True)
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todo.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
